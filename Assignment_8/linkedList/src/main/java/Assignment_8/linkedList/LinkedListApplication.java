c5fc3f73da825387dcfc25b053e78da648795971
황현서 <hhseo1202@naver.com>
2024-10-10 12:23:58 +0900

    initial

---
+++ Assignment_8/linkedList/src/main/java/Assignment_8/linkedList/LinkedListApplication.java
@@ -0,0 +1,13 @@
+package Assignment_8.linkedList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedListApplication.class, args);
+	}
+
+}
---
+++ Assignment_8/linkedList/src/test/java/Assignment_8/linkedList/LinkedListApplicationTests.java
@@ -0,0 +1,13 @@
+package Assignment_8.linkedList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
