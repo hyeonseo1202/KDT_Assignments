e49ec03d3dc32fcd07b38b7e0af744f8293ec2bb
황현서 <hhseo1202@naver.com>
2024-10-11 21:53:08 +0900

    feat

--- Assignment_8/linkedList/src/main/java/Assignment_8/linkedList/LinkedList.java
+++ Assignment_8/linkedList/src/main/java/Assignment_8/linkedList/LinkedList.java
@@ -6,17 +6,36 @@ public class LinkedList implements LinkedListInterface{
     public LinkedList(ListNode head) {
         this.head = head;
     }
+    //스택, 큐 전부 First-In이니까! - 마지막에 삽입
+    public void insertNode(String data){
+        ListNode newNode = new ListNode(data); //새로운 노드 생성
+        if(head == null){
+            this.head = newNode;
+        }else{
+            //head 노드가 null이 아닌 경우 temp 노드가 head 참조
+            //tempNode는 마지막 노드를 찾아서 참조하기 위해 사용
+            ListNode tempNode = head;
 
-    public void insertHeadNode(String data){
+            while(tempNode.link != null){
+                tempNode = tempNode.link; //다음 노드 참조
+            }
 
+
+        }
     }
-    public void insertTailNode(String data){
+    public void deleteTailNode(){
+
 
     }
     public void deleteFirstNode(){
+        ListNode preNode;
+        ListNode tempNode;
 
-    }
-    public void deleteLastNode(){
+        if(head == null){
+            return;
+        }else {
+            preNode = head;
+        }
 
     }
     public void deleteSpecificNode(String data){
--- Assignment_8/linkedList/src/main/java/Assignment_8/linkedList/LinkedListInterface.java
+++ Assignment_8/linkedList/src/main/java/Assignment_8/linkedList/LinkedListInterface.java
@@ -2,9 +2,8 @@ package Assignment_8.linkedList;
 
 public interface LinkedListInterface {
     public void insertHeadNode(String data);
-    public void insertTailNode(String data);
-    public void deleteFirstNode();
-    public void deleteLastNode();
+    public void deleteHeadNode();
+    public void deleteTailNode();
     public void deleteSpecificNode(String data);
     public void searchNode(String data);
     public void printList();
