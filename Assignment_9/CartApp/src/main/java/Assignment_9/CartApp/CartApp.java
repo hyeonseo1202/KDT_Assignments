c2712969346706929621eff10421309111659fc3
황현서 <hhseo1202@naver.com>
2024-10-25 00:28:45 +0900

    feat

--- Assignment_9/CartApp/src/main/java/Assignment_9/CartApp/CartApp.java
+++ Assignment_9/CartApp/src/main/java/Assignment_9/CartApp/CartApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class CartApp {
--- Assignment_9/CartApp/src/main/java/Assignment_9/CartApp/Product.java
+++ Assignment_9/CartApp/src/main/java/Assignment_9/CartApp/Product.java
@@ -1,4 +1,12 @@
 package Assignment_9.CartApp;
 
+
+import lombok.Getter;
+
+
+@Getter
 public class Product {
+    public String product;
+    public String name;
+
 }
